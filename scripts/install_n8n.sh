c781cdef5d923c2431b9190a81eefafebee182ad
clementalo9 <51920941+clementalo9@users.noreply.github.com>
2025-05-30 17:44:21 +0200

    Create install_n8n.sh

---
+++ scripts/install_n8n.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt update && sudo apt install -y ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
+  https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+newgrp docker
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+cd ~/n8n_oci
+mkdir n8n_data
+sudo chown -R 1000:1000 n8n_data
+docker-compose up -d
