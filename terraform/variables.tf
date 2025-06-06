80d72d9aeb8bd1346f461b765abc4cccc76295d6
Clement <clementgenest@yahoo.fr>
2025-06-06 18:35:37 +0200

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,15 @@
-variable "tenancy_ocid" {
-  description = "The OCID of the tenancy"
+variable "admin_username" {
+  description = "Username for accessing the application web interface"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Password for accessing the application web interface"
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key to access the instance"
   type        = string
 }
