aff222d82c057f773511cf100befa6d179a7d754
Clement <clementgenest@yahoo.fr>
2025-06-09 00:20:37 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,5 @@
-variable "admin_username" {
-  description = "Username for accessing the application web interface"
-  type        = string
-}
-
-variable "admin_password" {
-  description = "Password for accessing the application web interface"
-  type        = string
-  sensitive   = true
-}
 
 variable "ssh_public_key" {
-  description = "SSH public key to access the instance"
+  description = "SSH Public Key"
   type        = string
 }
