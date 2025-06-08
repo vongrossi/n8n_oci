6169e840eba41b6af1e02b5cf5e32e0cfb67fd43
Clement <clementgenest@yahoo.fr>
2025-06-09 00:28:52 +0200

    Update maint.tf

--- terraform/maint.tf
+++ terraform/maint.tf
@@ -1,22 +1,8 @@
 provider "oci" {}
 
-variable "ssh_public_key" {
-  description = "SSH public key for the instance"
-  type        = string
-}
 
-variable "n8n_admin_user" {
-  description = "n8n Basic Auth Username"
-  type        = string
-  default     = "admin"
-}
 
-variable "n8n_admin_password" {
-  description = "n8n Basic Auth Password"
-  type        = string
-  default     = "adminpassword"
-  sensitive   = true
-}
+
 
 variable "tenancy_ocid" {
   description = "The OCID of the tenancy"
