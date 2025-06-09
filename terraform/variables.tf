36dfe7b42d1b59bd20215314c1dd5380ba51a0ea
clementalo9 <51920941+clementalo9@users.noreply.github.com>
2025-06-09 11:48:50 +0200

    Fix undefined admin variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,14 @@ variable "ssh_public_key" {
   description = "SSH Public Key"
   type        = string
 }
+
+variable "n8n_admin_user" {
+  description = "n8n admin username"
+  type        = string
+}
+
+variable "n8n_admin_password" {
+  description = "n8n admin password"
+  type        = string
+  sensitive   = true
+}
